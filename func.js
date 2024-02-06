0bfd368c768690c52dd65eb6830c2da3d1b41bed
W Souza <dev9.ws@gmail.com>
2024-02-06 19:24:23 -0300

    initial

---
+++ func.js
@@ -0,0 +1,9 @@
+"use strict";
+function mult(num1, num2) {
+    return num1 * num2;
+}
+function ola(nome) {
+    return `Olá, ${nome}`;
+}
+console.log(`${mult(5, 3)}`);
+console.log(`${ola("Souza")}`);
---
+++ func.ts
@@ -0,0 +1,10 @@
+function mult(num1: number, num2: number ): number{
+    return num1 * num2;
+}
+
+function ola(nome: string):string {
+    return `Olá, ${nome}`
+}
+
+console.log(`${mult(5, 3)}`);
+console.log(`${ola("Souza")}`);
